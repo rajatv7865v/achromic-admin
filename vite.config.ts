8dde0e6791a8024e9f6eab76a97d0534eec35cb6
Rajat Verma <rajatverma@Rajats-MacBook-Pro.local>
2025-12-24 12:14:10 +0530

    add

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: true,
+    port: 5173,
+    allowedHosts: ['admin.sharkwaveinfo.com'],
+  },
 });
 
 
